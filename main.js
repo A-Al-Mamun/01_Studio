b2b36ab40fe8e34268db5eb9b14076d94ec8a57c
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-24 23:57:41 +0600

    Working with event click in menu

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+let menu = document.getElementsByClassName('menu')[0];
+
+menu.addEventListener('click', () => {
+    let menuItems = document.getElementsByClassName('menu-items')[0];
+
+    menuItems.style.tranforme = 'TranslateX: 250px';
+})
