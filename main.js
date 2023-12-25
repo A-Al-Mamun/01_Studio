8de6e57e78c9c20f658f618216ee8fc7ad8f0d82
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-12-25 22:07:01 +0600

    01 studio nav, info, share btn event work completed

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
-
+// Nav Bar Menu
 let menu = document.getElementsByClassName('menu')[0];
-
-menu.addEventListener('click', () => {
+menu.addEventListener("click", () => {
     let menuItems = document.getElementsByClassName('menu-items')[0];
+    if(menuItems.style.right != "50px"){
+        menuItems.style.right = "50px";
+        menuItems.style.zIndex = "11";
+    }else{
+        menuItems.style.right = "-200px";
+    }
+});
 
-    menuItems.style.tranforme = 'TranslateX: 250px';
-})
+
+// Share Section 
+let shareBtn = document.getElementsByClassName('share-btn')[0];
+shareBtn.addEventListener("click", () => {
+    let shareSec = document.getElementsByClassName('share')[0];
+    if(shareSec.style.right != "40px"){
+        shareSec.style.right = "40px";
+        shareSec.style.zIndex = "10";
+    }else{
+        shareSec.style.right = "-400px";
+    }
+});
+
+// Info Section 
+let infoBtn = document.getElementsByClassName('info-btn')[0];
+infoBtn.addEventListener("click", () => {
+    let infoSec = document.getElementsByClassName('info')[0];
+    if(infoSec.style.right != "40px"){
+        infoSec.style.right = "40px";
+        infoSec.style.zIndex = "10";
+    }else{
+        infoSec.style.right = "-400px";
+    }
+});
